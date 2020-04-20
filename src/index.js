d821559960d0abaf37fe5e866cbef85141464a66
tvergho <tvergho@gmail.com>
2020-04-19 20:39:19 -0700

    react support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let sec = 0;
-window.setInterval(() => {
-    $('#main').html(`You've been on this page for ${sec} seconds.`);
-    sec += 1;
-}, 1000);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
